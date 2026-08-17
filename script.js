1cec745b0f29c274f2e82bb59ac4f1120463a6db
Filip Nowak <filipnowakkoscian@gmail.com>
2026-08-17 19:54:17 +0200

    Initialize basic calculator skeleton with arithmetic functions

---
+++ script.js
@@ -0,0 +1,26 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+let numA = 0;
+let numB = 0;
+let operator = '';
+
+//Calls one of the functions above
+function operate(numA, numB, operator) {
+
+}
+
+
